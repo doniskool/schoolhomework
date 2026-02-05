75e3270311269b6324fa0d071e88b91b44faecf1
doniskool <116699966+doniskool@users.noreply.github.com>
2026-02-05 10:54:18 -0500

    The code.py
    Added variables of different data types and print statements for homework assignment.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+k = "Donavon malave"
+summer = "during summer of 2024"
+l = "lucky"
+g = 3.3
+
+# this is the actual code the output will be below. 
+# the homework said to use 5 different variables of different data types ie; float, booleans etc
+# I do not know If I followed requirements exactly.
+print(f"hello my name is {k} I am currently a junior."), print(f"So far I will admit I have not learned much as I did use a little bit of c+ {summer} I was not able to make anything advancd but I have made a calculator using python and c+ so far, variables do work a little differently in python I have noticed that the var commands do not act the same. Using print is the simplest part of coding so Im not really confused on anything like that I am trying to currently figure out the float command as it's one of the more simple commands that I have not yet fully understood I do have a pet named {l} he is a dog and has helped with me coding unironically, as sometimes he would make it less stressful to code and I could just pet him to relieve stress since coding is sometimes annoying. This does not mean I don't want to learn to code better I feel like I could definetly become better at coding and better at doing work in general as I currently only have a gpa of {g} and that is quite saddening to me."), print("Overall this is quite saddening to me as I feel like I could do better but at the same time I feel somewhat bored in class due to me having more knowledge than others to an extent."), print(input("does this pass the homework requirements? (yes, no)"))
+
+
+
 
